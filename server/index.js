fdb03ec36f2133147f698291fa199568bc89c325
Sahil Totala <81063988+Flamingloon@users.noreply.github.com>
2022-12-21 21:24:56 +0530

    Created Rest api and database connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const monngoose = require("mongoose");
+const authRouter = require("./routes/auth");
 
 const PORT = process.env.PORT | 3001 ;
 
 const DB ="mongodb+srv://sahil:sahil@cluster0.qdlhptd.mongodb.net/?retryWrites=true&w=majority";
+const app = express();
 
+app.use(express.json());
+app.use(authRouter);
 monngoose.connect(DB).then(() =>
   {
     console.log("Connection Successfull");
@@ -13,7 +17,7 @@ monngoose.connect(DB).then(() =>
     console.log(err);
 });
 
-const app = express();
+
 
 app.listen(PORT , "0.0.0.0", () => {
     console.log(`Connected to port ${PORT} is lit`);
