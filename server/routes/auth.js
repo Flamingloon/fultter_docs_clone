ee9130e49b0e855e70e58a9f2a7db1bda59153f5
Sahil Totala <81063988+Flamingloon@users.noreply.github.com>
2022-12-29 12:33:46 +0530

    added Tokens

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,33 +1,35 @@
 const express = require("express");
 const User = require("../models/user");
-
+const jwt = require("jsonwebtoken");
+const auth = require("../middlewares/auth");
 const authRouter = express.Router();
 
-authRouter.post('/api/signup', async (req,res) => {
+authRouter.post("/api/signup", async (req, res) => {
+  try {
+    const { name, email, profilePic } = req.body;
 
-    try{
+    let user = await User.findOne({ email });
 
-        const { name , email , profilePic} = req.body;
-        let user = await User.findOne({ email });
-
-        if(!user){
-            user = new User({
-                email,
-                profilePic,
-                name,
-            });
-
-            user = await user.save();
-        }
-        res.json({user} );
-    } catch(e) {
-        res.status(500).json({ error: e.message });
-
+    if (!user) {
+      user = new User({
+        email,
+        profilePic,
+        name,
+      });
+      user = await user.save();
     }
-})
 
-authRouter.post('/api/signup', (req,res) => {
-  
-}) 
+    const token = jwt.sign({ id: user._id }, "passwordKey");
+
+    res.json({ user, token });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+authRouter.get("/", auth, async (req, res) => {
+  const user = await User.findById(req.user);
+  res.json({ user, token: req.token });
+});
 
 module.exports = authRouter;
